99fb64b69164bbdd710b2e45298e30b2de58570e
aleksey.zubov <aleksey.zubov@rt.ru>
2020-08-29 12:42:22 +0300

    21 урок, добавил стилизация для Dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,11 +1,29 @@
 import React from "react";
-// import css from './Dialogs.module.css'
+
+import css from './Dialogs.module.css'
 
 
 function Dialogs() {
     return (
         <div>
-            Dialogs
+            <div className={css.dialogs}>
+                <div className={css.dialogs_items}>
+                    <div className={css.dialog + ' ' + css.active}>
+                        Alex
+                    </div>
+                    <div className={css.dialog}>
+                        Sveta
+                    </div>
+                    <div className={css.dialog}>
+                        Jora
+                    </div>
+                </div>
+                <div className={css.messages}>
+                    <div className={css.message}>Hi</div>
+                    <div className={css.message}>Yooo</div>
+                    <div className={css.message}>Privet</div>
+                </div>
+            </div>
         </div>
     )
 }
